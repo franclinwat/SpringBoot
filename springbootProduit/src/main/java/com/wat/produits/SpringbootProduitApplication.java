948e32f8938eebdb0f1d338c932eb85c94863ee5
watchou <franclinwat@yahoo.fr>
2022-04-01 03:33:00 +0300

    premier push and commit

---
+++ springbootProduit/src/main/java/com/wat/produits/SpringbootProduitApplication.java
@@ -0,0 +1,13 @@
+package com.wat.produits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProduitApplication.class, args);
+	}
+
+}
---
+++ springbootProduit/src/test/java/com/wat/produits/SpringbootProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wat.produits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
