d6b0fa1e40483acf0ab94f6fa29a6c9652bbedf6
watchou <franclinwat@yahoo.fr>
2022-04-05 08:41:41 +0300

    controller

--- springbootProduit/src/main/java/com/wat/produits/controllers/ProduitsController.java
+++ springbootProduit/src/main/java/com/wat/produits/controllers/ProduitsController.java
@@ -1,5 +1,18 @@
 package com.wat.produits.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wat.produits.service.ProduitService;
+
 public class ProduitsController {
+	@Autowired
+	ProduitService produitService;
+	
+	@RequestMapping("/showCreate")
+	public String showCreate() {
+		
+		return "createProduit";
+	}
 
 }
